414e7e4628ca97ec4ebb6961b130820c4ae92b05
Mark Small <m.small@ed.ac.uk>
2018-02-26 23:06:02 +0000

    Setup Redux and Asynchronous API interactions:
    Added Redux to manage state. I've also added a number of dependencies
    to provide the ability to call remote APIs, as well as some debugging
    tools to make Redux development easier.
    
    Added code to index.js to setup the middleware (thunk) to be used to
    create async action creators used to make requests of remote APIs. It
    also adds the previously mentioned development tools (in `development`
    mode only), these are all used to create the Redux store used in the
    app.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from 'registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 1. Setup store to use middleware (thunk) to create API calls.
+// 2. Add redux-logger to middleware.
+const middleware = [thunk];
+middleware.push(createLogger());
+
+let store;
+if (process.env.NODE_ENV === 'development') {
+  // 1. Add redux dev tools (development mode only).
+  // 2. Create store composed of reducers and middleware.
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  store = createStore(
+    composeEnhancers(applyMiddleware(...middleware))
+  );
+} else {
+  // 1. Create store composed of reducers and middleware.
+  store = createStore(applyMiddleware(...middleware));
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
