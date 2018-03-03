7f299ddfac965ce1f804a7e0a7beb0faf4a4dea8
Mark Small <m.small@ed.ac.uk>
2018-03-03 23:58:26 +0000

    Add Generic Form validation:
    Created a Generic Form validation system that can be used against any
    form. It uses a Higher Order Component (HOC) function to map state and
    validation rules to a Stateless Functional Component that in this case
    is the LoginForm.
    
    This solution was found at:
    https://medium.com/javascript-inside/form-validation-as-a-higher-order-component-pt-2-1edb7881870d
    
    I've tweaked it a bit, mainly splitting up concerns a bit more and moving
    from data.either to folktale.
    
    I've removed the connecting of the form with Redux, so submitting no
    longer works (in fact there is a bug at the moment with the onSubmit
    function I've implememented, I'll be fixing that next, then re-adding
    the connection to Redux.

--- src/login/login.component.js
+++ src/login/login.component.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
 
 import styles from './login.module.css';
+import LoginForm from './login-form.component';
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      email: '',
-      password: ''
-    };
-    this.loggingIn = false;
-  }
+  // //   this.state = {
+  // //     email: '',
+  // //     password: ''
+  // //   };
+  // //   this.loggingIn = false;
+  // }
 
-  handleChange = event => {
-    console.log('Handling change to form: ', event);
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
+  // handleChange = event => {
+  //   console.log('Handling change to form: ', event);
+  //   const { name, value } = event.target;
+  //   this.setState({ [name]: value });
+  // };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.loggingIn = true;
-    this.props.login(this.state);
-  };
+  // handleSubmit = event => {
+  //   event.preventDefault();
+  //   this.loggingIn = true;
+  //   this.props.login(this.state);
+  // };
 
   render() {
     return (
@@ -36,45 +37,7 @@ class Login extends Component {
                 <a href="#">Need an account?</a>
               </p>
 
-              <form onSubmit={e => this.handleSubmit(e)}>
-                <fieldset>
-                  <fieldset className="form-group">
-                    <input
-                      type="email"
-                      name="email"
-                      className="form-control form-control-lg"
-                      value={this.state.email}
-                      onChange={this.handleChange}
-                      placeholder="Email"
-                    />
-                  </fieldset>
-
-                  <fieldset className="form-group">
-                    <input
-                      type="password"
-                      name="password"
-                      value={this.state.password}
-                      onChange={this.handleChange}
-                      className="form-control form-control-lg"
-                      placeholder="Password"
-                    />
-                  </fieldset>
-
-                  <button
-                    type="submit"
-                    name="email"
-                    className="btn btn-lg btn-primary pull-xs-right"
-                  >
-                    Sign in
-                  </button>
-                  {this.loggingIn && (
-                    <img
-                      src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="
-                      alt="spinner"
-                    />
-                  )}
-                </fieldset>
-              </form>
+              <LoginForm />
             </div>
           </div>
         </div>
--- src/login/login.container.js
+++ src/login/login.container.js
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 import Login from './login.component';
 import { doLogin } from './login.actions';
 
-const mapDispatchToProps = dispatch => {
-  return {
-    login: form => {
-      dispatch(doLogin(form));
-    }
-  };
-};
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     login: form => {
+//       dispatch(doLogin(form));
+//     }
+//   };
+// };
 
-export default connect(() => ({}), mapDispatchToProps)(Login);
+// export default connect(() => ({}), mapDispatchToProps)(Login);
+export default Login;
