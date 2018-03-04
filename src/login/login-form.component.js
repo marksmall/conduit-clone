996290cd1db601d41f57309e5c37ae4518055d07
Mark Small <m.small@ed.ac.uk>
2018-03-04 02:12:36 +0000

    Tidy up the login form component:
    * Removed whitespace to fix linting errors.
    * Removed commented out code.
    * Correct the error message text.
    * Commented out code that no longer works (will re-introduce it later).

--- src/login/login-form.component.js
+++ src/login/login-form.component.js
@@ -18,10 +18,10 @@ const LoginForm = ({ form, onChange, submit, errors = {} }) => (
         value={form.username}
         onChange={R.compose(onChange('username'), getValue)}
         placeholder="Email"
+        autoFocus
       />
       {errors.username}
     </fieldset>
-
     <fieldset className="form-group">
       <input
         type="password"
@@ -33,7 +33,6 @@ const LoginForm = ({ form, onChange, submit, errors = {} }) => (
       />
       {errors.password}
     </fieldset>
-
     <button
       type="submit"
       className="btn btn-lg btn-primary pull-xs-right"
@@ -41,43 +40,20 @@ const LoginForm = ({ form, onChange, submit, errors = {} }) => (
     >
       Sign in
     </button>
-    {this.loggingIn && (
+    {/* {this.loggingIn && (
       <img
         src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="
         alt="spinner"
-      />
-    )}
-
-    {/* <div className="formGroup">
-      <label htmlFor="name">Name</label>
-      <input
-        id="name"
-        type="text"
-        value={form.name}
-        onChange={R.compose(onChange('name'), getValue)}
-      />
-      {errors.name}
-    </div>
-    <div className="formGroup">
-      <label htmlFor="random">Random</label>
-      <input
-        id="random"
-        type="text"
-        value={form.random}
-        onChange={R.compose(onChange('random'), getValue)}
-      />
-      {errors.random}
-    </div>
-    <button onClick={() => onSubmit(form)}>Submit</button> */}
+      /> */}
   </fieldset>
 );
 
 const validationRules = {
-  username: [[isNotEmpty, 'Name should not be  empty.']],
+  username: [[isNotEmpty, 'Username should not be  empty.']],
   // password: [[isNotEmpty, 'Name should not be  empty.']]
   password: [
-    [isLengthGreaterThan(7), 'Minimum Random length of 8 is required.'],
-    [hasCapitalLetter, 'Random should contain at least one uppercase letter.']
+    [isLengthGreaterThan(7), 'Minimum Password length of 8 is required.'],
+    [hasCapitalLetter, 'Password should contain at least one uppercase letter.']
   ]
 };
 
@@ -88,5 +64,4 @@ const initialState = {
   }
 };
 
-const enhanced = withForm(initialState, validationRules);
-export default enhanced(LoginForm);
+export default withForm(initialState, validationRules)(LoginForm);
