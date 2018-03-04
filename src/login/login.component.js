f8c08bc1e90f5e9edcc3d918d2bcc9f89a9519fa
Mark Small <m.small@ed.ac.uk>
2018-03-04 02:11:12 +0000

    Refactor Login component to be Stateless:
    Previously this component was Stateful, I've since refactored it but
    forgot to change it from a class to a function, which I've now done.

--- src/login/login.component.js
+++ src/login/login.component.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import styles from './login.module.css';
 import LoginForm from './login-form.component';
 
-class Login extends Component {
-  // handleSubmit = event => {
-  //   event.preventDefault();
-  //   this.loggingIn = true;
-  //   this.props.login(this.state);
-  // };
+const Login = props => {
+  return (
+    <div className={styles['auth-page']}>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign In</h1>
+            <p className="text-xs-center">
+              <a href="#">Need an account?</a>
+            </p>
 
-  render() {
-    return (
-      <div className={styles['auth-page']}>
-        <div className="container page">
-          <div className="row">
-            <div className="col-md-6 offset-md-3 col-xs-12">
-              <h1 className="text-xs-center">Sign In</h1>
-              <p className="text-xs-center">
-                <a href="#">Need an account?</a>
-              </p>
-
-              <LoginForm props={this.props} />
-            </div>
+            <LoginForm props={this.props} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Login;
